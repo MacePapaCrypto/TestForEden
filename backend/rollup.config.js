2d97c572d3758a7ba71b633625c8ffaedb7416a3
edencoder <alex@edenup.com>
2022-03-28 13:58:39 +1000

    add backend rollup, fix entry file

---
+++ backend/rollup.config.js
@@ -0,0 +1,65 @@
+import pkg from './package.json';
+import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import builtIns from 'rollup-plugin-node-builtins';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDeps from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+// rollup
+const production = !process.env.ROLLUP_WATCH;
+
+// extensions
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+// export default
+export default {
+  input  : 'src/index.ts',
+  output : [
+    {
+      file      : pkg.main,
+      format    : 'esm',
+      sourcemap : true,
+    },
+  ],
+  plugins : [
+    builtIns(),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser : true,
+      extensions,
+    }),
+    commonjs({
+      include : [
+        'node_modules/**',
+      ],
+      exclude : [
+        'node_modules/process-es6/**',
+      ],
+    }),
+    json(),
+    babel({
+      extensions,
+      babelrc : true,
+      include : ['src/**/*'],
+      presets : [
+        '@babel/preset-env',
+        '@babel/preset-typescript',
+      ],
+    }),
+    peerDeps(),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch : {
+    clearScreen : false,
+  },
+};
