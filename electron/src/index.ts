cfd84cbcffefbc73d499d48242bb579c75711928
edencoder <alex@edenup.com>
2022-06-28 15:36:55 +1000

    build initial electron class

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -3,39 +3,53 @@
 import path from 'path';
 import { app, BrowserWindow } from 'electron';
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width  : 800,
-    height : 600,
-  });
+/**
+ * create moon electron class
+ */
+class MoonElectron {
+  /**
+   * construct moon electron
+   */
+  constructor() {
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+    // run
+    this.run();
+  }
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(`${__dirname}/index.html`)
+  /**
+   * runs moon electron
+   */
+  async run() {
+    // await app ready
+    await app.whenReady();
+
+    // on all closed
+    app.on('window-all-closed', () => {
+      // quit app
+      if (process.platform !== 'darwin') app.quit();
+    });
+
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+      width  : 800,
+      height : 600,
+  
+      webPreferences : {
+        nodeIntegration  : true,
+        contextIsolation : false,
+      },
+    });
+  
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+  
+    // and load the index.html of the app.
+    mainWindow.loadFile(`${__dirname}/index.html`)
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+// exporting
+const exporting = new MoonElectron();
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// export default
+export default exporting;
